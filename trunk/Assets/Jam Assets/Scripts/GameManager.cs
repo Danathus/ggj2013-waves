9d81af9c0e1710d2ba0a117f5d118c9e9646b470
asyskin@ucsc.edu <asyskin@ucsc.edu@690c82a3-eaa7-7cb1-63d8-8883d1f090f1>
2013-01-26 21:11:32 +0000

    Sounds are in.

--- trunk/Assets/Jam Assets/Scripts/GameManager.cs
+++ trunk/Assets/Jam Assets/Scripts/GameManager.cs
@@ -5,13 +5,22 @@ public class GameManager : MonoSingleton<GameManager> {
 	
 	public float testValue = 1.0f;
 
+	AudioClip heartBeat;
+	
+	
 	// Use this for initialization
 	void Start () {
-	
+		
+		heartBeat = (AudioClip)Resources.Load ("GGJ13_Theme", typeof(AudioClip));
+		audio.clip = heartBeat;
+		audio.Play();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+
 	
 	}
 }
