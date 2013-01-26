8eb7ca7f4e25737a9d0ab6d9a4bc8a5e3fff5d08
Danathus <Danathus@users.noreply.github.com>
2013-01-26 06:36:49 +0000

    now procedurally generating some geometry

--- Assets/Jam Assets/Test/TestScript.cs
+++ Assets/Jam Assets/Test/TestScript.cs
@@ -10,6 +10,14 @@ public class TestScript : MonoBehaviour {
 		
 	}
 
+	// djmc -- make these public to expose to unity editor
+	private Vector3 point = Vector3.up;
+	private int numberOfPoints = 10;
+
+	private Mesh mesh;
+	private Vector3[] vertices;
+	private int[] triangles;
+
 	// Use this for initialization
 	void Start () {
 		Camera.main.transform.position += Vector3.back * 10;
@@ -23,6 +31,29 @@ public class TestScript : MonoBehaviour {
 			player[i] = (GameObject)GameObject.Instantiate(spherePrefab);
 			player[i].transform.position += Vector3.right * i;
 		}
+
+		// create a star mesh
+		mesh = new Mesh();
+		mesh.name = "Star Mesh";
+		vertices = new Vector3[numberOfPoints + 1];
+		triangles = new int[numberOfPoints * 3];
+		float angle = -360f / numberOfPoints;
+		for(int v = 1, t = 1; v < vertices.Length; v++, t += 3){
+			vertices[v] = Quaternion.Euler(0f, 0f, angle * (v - 1)) * point;
+			triangles[t] = v;
+			triangles[t + 1] = v + 1;
+		}
+		triangles[triangles.Length - 1] = 1;
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		
+		GameObject go = new GameObject(); //GameObject.Instantiate();
+		MeshFilter mf = (MeshFilter)go.AddComponent(typeof(MeshFilter));
+		mf.mesh = mesh;
+		GameObject.Instantiate(go);
+		go.AddComponent(typeof(MeshRenderer));
+		go.name = "Wave";
 	}
 	
 	// Update is called once per frame
