1222da3347c70f04baef787ae77f5e809d8249cc
asyskin <asyskin@ucsc.edu>
2013-01-27 06:44:56 +0000

    Added player selection screen and tried to modify everything that depends on colors.

---
+++ Assets/Jam Assets/Scripts/PlayerSel.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerSel : MonoBehaviour {
+	
+	private bool toggleRed = true;
+	private bool toggleBlue = true;
+	private bool toggleGreen = true;
+	private bool toggleYellow = true;
+	
+	void OnGUI(){
+		
+		toggleRed = GUI.Toggle (new Rect (Screen.width/2-40, Screen.height/2 - 20, 100, 30), toggleRed, "Red Player");
+		toggleBlue = GUI.Toggle (new Rect (Screen.width/2-40, Screen.height/2 + 20, 100, 30), toggleBlue, "Blue Player");
+		toggleGreen = GUI.Toggle (new Rect (Screen.width/2-40, Screen.height/2 + 60, 100, 30), toggleGreen, "Green Player");
+		toggleYellow = GUI.Toggle (new Rect (Screen.width/2-40, Screen.height/2 + 100, 100, 30), toggleYellow, "Yellow Player");
+		
+		if (GUI.Button (new Rect (Screen.width / 4 + 60, Screen.height /2 - 70, 140, 50), "Play")) {
+			PlayerPrefs.DeleteAll();
+			if(toggleRed)
+				PlayerPrefs.SetString("Player2", "Red");
+			if(toggleBlue)
+				PlayerPrefs.SetString ("Player0", "Blue");
+			if(toggleGreen)
+				PlayerPrefs.SetString("Player1", "Green");
+			if(toggleYellow)
+				PlayerPrefs.SetString("Player3", "Yellow");
+			Application.LoadLevel("TestScene");
+		}
+	}
+	
+}
--- Assets/Jam Assets/Scripts/WaveField.cs
+++ Assets/Jam Assets/Scripts/WaveField.cs
@@ -82,7 +82,7 @@ public class WaveField
 	}
 	
 	// ------------------------------------------------------------------------
-	public void SetPressure(Vector2 screenCoordinates, int pressure, int playerID) // think big, powers of 2
+	public void SetPressure(Vector2 screenCoordinates, int pressure, Color colorCode) // think big, powers of 2
 	{
 		// todo: need to convert between real-space and grid-space
 		//    question: how many units to the edge of the screen? Can we affix that to something specific?
@@ -95,13 +95,14 @@ public class WaveField
 		// If we're in bounds apply the pressure
 		if (index < tmpState1.Length) {
 			if(counter % 2 == 0) {
-				switch (playerID) {
+				
+				/*switch (playerID) {
 					case 0: tmpState1[index].red = pressure; break;
 					case 1: tmpState1[index].green = pressure; break;
 					case 2: tmpState1[index].blue = pressure; break;
 					case 3: tmpState1[index].yellow = pressure; break;
 					default: break;
-				}
+				}*/
 	            
 			}
 	        else {
