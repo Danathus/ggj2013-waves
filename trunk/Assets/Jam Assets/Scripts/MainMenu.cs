be323fcdf4d93f0b9e1b107607cb8e97685d4c84
asyskin@ucsc.edu <asyskin@ucsc.edu@690c82a3-eaa7-7cb1-63d8-8883d1f090f1>
2013-01-27 04:45:52 +0000

    Added main menu scene that does nothing.

---
+++ trunk/Assets/Jam Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	private bool toggleRed = true;
+	private bool toggleBlue = true;
+	private bool toggleGreen = true;
+	private bool toggleYellow = true;
+	
+	void OnGUI () {
+		
+		GUI.Label (new Rect (Screen.width/2 - 30, Screen.height/4 - 50, 100, 60), "Waves");
+		
+		/*toggleRed = GUI.Toggle (new Rect (Screen.width/2-40, Screen.height/2 - 20, 100, 30), toggleRed, "Red Player");
+		toggleBlue = GUI.Toggle (new Rect (Screen.width/2-40, Screen.height/2 + 20, 100, 30), toggleBlue, "Blue Player");
+		toggleGreen = GUI.Toggle (new Rect (Screen.width/2-40, Screen.height/2 + 60, 100, 30), toggleGreen, "Green Player");
+		toggleYellow = GUI.Toggle (new Rect (Screen.width/2-40, Screen.height/2 + 100, 100, 30), toggleYellow, "Yellow Player");*/
+		
+		GUI.Label (new Rect(Screen.width/4, Screen.height/2 + 40, 300,40), "Note: Play this game with Xbox 360 controllers.");
+		
+		GUI.Label (new Rect(Screen.width/8, Screen.height - 70, 440, 40), "Made by: Alexandr Syskin, Danny McCue, and Michael Guerrero");
+		
+		if (GUI.Button (new Rect (Screen.width / 4 + 60, Screen.height /2 - 70, 140, 50), "Character Selection")) {
+			if(Input.GetButton("A_1"))
+				GUI.FocusControl("Play");
+			//Application.LoadLevel("TestScene");
+			//print ("You clicked the button!");
+			//Popup.List (new Rect(10,10,150,100),ref showList, ref menuOpt, content, popUpList, GUIStyle.none, callBack);
+		}
+		
+	}
+	
+	public void callBack(){
+	}
+}
